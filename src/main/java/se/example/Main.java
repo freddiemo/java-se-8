a255795bc481be4f623a3adb1f90203d0d5b6c2c
Freddie Molina <freddiejmo@gmail.com>
2023-05-14 17:50:27 -0500

    1-operators, operands and expressions

--- src/main/java/se/example/Main.java
+++ src/main/java/se/example/Main.java
@@ -1,10 +1,26 @@
 package se.example;
 
 /**
- * Hello World sample
+ * Operators, operands and expressions samples.
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello World!");
+        int result = 1 + 2; // 1 + 2 = 3
+        System.out.println("Result " + result);
+        int previousResult = result;
+        System.out.println("Previous result " + previousResult);
+        result = result - 1; // 3 - 1 = 2
+        System.out.println("Result " + result);
+        char firstChar = 'A'; char secondChar = 'B';
+        System.out.println(firstChar + secondChar);
+        System.out.println("" + firstChar + secondChar);
+        result = 2;
+        result = result * 10; // 2 * 10 = 20
+        System.out.println(result);
+        result = result / 4; // 40 / 4 = 5
+        System.out.println(result);
+        result = 5;
+        result = result % 3; // the remainder of (5 % 3) = 2
+        System.out.println(result);
     }
 }
