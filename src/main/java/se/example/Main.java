60d900dda62235a71272395a70058f2757e864d8
Freddie Molina <freddiejmo@gmail.com>
2023-05-13 21:10:21 -0500

    added sample expressions

--- src/main/java/se/example/Main.java
+++ src/main/java/se/example/Main.java
@@ -7,15 +7,15 @@ import org.omg.Messaging.SyncScopeHelper;
  */
 public class Main {
     public static void main(String[] args) {
-        int myFirstNumber = 5;
-        System.out.println(myFirstNumber);
-        myFirstNumber = 10;
-        System.out.println(myFirstNumber);
-        myFirstNumber = 1000;
-        System.out.println(myFirstNumber);
-        myFirstNumber = 10 + 5;
-        System.out.println(myFirstNumber);
-        myFirstNumber = (10 + 5) + (2 * 10);
-        System.out.print(myFirstNumber);
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = 6;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        System.out.println(myTotal);
+        myThirdNumber = myFirstNumber * 2;
+        myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        System.out.println(myTotal);
+        int myLastOne = 1000 - myTotal;
+        System.out.println(myLastOne);
     }
 }
