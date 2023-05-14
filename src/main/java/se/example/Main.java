99d860f51e9bb6485cec381c003af81d746118e3
Freddie Molina <freddiejmo@gmail.com>
2023-05-14 15:39:45 -0500

    4-float and double primitives

--- src/main/java/se/example/Main.java
+++ src/main/java/se/example/Main.java
@@ -2,29 +2,47 @@ package se.example;
 
 /**
  * Primitive Types sample
+ * In Java the default type for integers is int.
+ * The default type for floating numbers is double.
  */
 public class Main {
     public static void main(String[] args) {
-        short myMinShortValue = Short.MIN_VALUE; int myMinIntValue = Integer.MIN_VALUE;
-        System.out.println(myMinShortValue);
-        System.out.println(myMinIntValue);
-        byte myMinByteValue = Byte.MIN_VALUE, myMaxByteValue = Byte.MAX_VALUE;
-        System.out.println(myMinByteValue);
-        System.out.println(myMaxByteValue);
-        // short firstShort = 1, int firstInteger = 2;
-        // byte firstByte = 1, byte secondByte = 2;
-        int myTotal = (myMinIntValue / 2);
-        System.out.println(myTotal);
-        byte myNewByteValue = (byte) (myMinByteValue / 2);
-        System.out.println(myNewByteValue);
-        short myNewShortValue = (short) (myMinShortValue / 2);
-        System.out.println(myNewShortValue);
-        byte myByte = 1;
-        short myShort = 2;
-        int myInt = 3;
-        long myLong = 50000L + (myByte + myShort + myInt) * 10;
-        System.out.println(myLong);
-        short shortTotal = (short) (1000 + 10 * (myByte + myShort + myInt));
-        System.out.println(shortTotal);
+        System.out.println("Float Value Range(" + Float.MIN_VALUE + " to " + Float.MAX_VALUE);
+        System.out.println("Double Value Range( " + Double.MIN_VALUE + " to " + Float.MAX_VALUE);
+        int myIntValue = 5; float myFloatValue = 5; double myDoubleValue = 5;
+        System.out.println("myIntValue " + myIntValue);
+        System.out.println("myFloatValue " + myFloatValue);
+        System.out.println("myDoubleValue " + myDoubleValue);
+        myFloatValue = 5f;
+        System.out.println("myFloatValue " + myFloatValue);
+        myDoubleValue = 5d;
+        System.out.println("myDoubleValue " + myDoubleValue);
+        // float myOtherFloatValue = 5.25;
+        float myOtherFloatValue = (float) 5.25; //not recommended
+        System.out.println("myOtherValue " + myOtherFloatValue);
+        myOtherFloatValue = 5.25f;
+        System.out.println("myOtherValue " + myOtherFloatValue);
+        myIntValue = 5 / 2;
+        System.out.println(myIntValue);
+        myFloatValue = 5f / 2f;
+        System.out.println(myFloatValue);
+        myDoubleValue = 5d / 2d;
+        System.out.println(myDoubleValue);
+        myIntValue = 5 / 3;
+        System.out.println(myIntValue);
+        myFloatValue = 5f / 3f;
+        System.out.println(myFloatValue);
+        myDoubleValue = 5d / 3d;
+        System.out.println(myDoubleValue);
+        myDoubleValue = 5.00 / 3.00;
+        System.out.println(myDoubleValue);
+        myDoubleValue = 5.00 / 3;
+        System.out.println(myDoubleValue);
+        // myFloatValue = 5.00 / 3f;
+        double pounds = 200d;
+        double kilograms = pounds * 0.45359237d;
+        System.out.println("Converted kilograms = " + kilograms);
+        double pi = 3.1415927d;
+        System.out.println(pi);
     }
 }
