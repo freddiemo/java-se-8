aa0ef46c8ea6de841fb22bfefba87d529a599536
Freddie Molina <freddiejmo@gmail.com>
2023-05-13 22:41:05 -0500

    2-byte, short and long primitive types

--- src/main/java/se/example/Main.java
+++ src/main/java/se/example/Main.java
@@ -5,16 +5,19 @@ package se.example;
  */
 public class Main {
     public static void main(String[] args) {
-        int myValue = 10000;
-        System.out.println(myValue);
-        int myMinIntValue = Integer.MIN_VALUE;
-        System.out.println("Integer minimum value: " + myMinIntValue);
-        int myMaxIntValue = Integer.MAX_VALUE;
-        System.out.println("Integer maximum value: " + myMaxIntValue);
-        System.out.println("Integer Value Range (" + Integer.MIN_VALUE + " to " + Integer.MAX_VALUE + ")");
-        System.out.println("Busted Max value = " + (myMaxIntValue + 1)); // overflow wraparound
-        System.out.println("Busted Min value = " + (myMinIntValue - 1)); // underflow wraparound
-        int myMaxIntTest = 2_147_483_647;
-        System.out.println(myMaxIntTest);
+        System.out.println("A Byte has a width of " + Byte.SIZE);
+        System.out.println("Byte Value Range ( " + Byte.MIN_VALUE + " to " + Byte.MAX_VALUE + ")");
+        System.out.println("A Short has a width of " + Short.SIZE);
+        System.out.println("Short Value Range ( " + Short.MIN_VALUE + " to " + Short.MAX_VALUE + ")");
+        System.out.println("A Integer has a width of " + Integer.SIZE);
+        System.out.println("Integer Value Range ( " + Integer.MIN_VALUE + " to " + Integer.MAX_VALUE + ")");
+        System.out.println("A long has a width of " + Long.SIZE);
+        System.out.println("Long Value Range ( " + Long.MIN_VALUE + " to " + Long.MAX_VALUE + ")");
+        long myLongValue = 100l;
+        System.out.println(myLongValue);
+        // myLongValue = 2_147_483_647_234;
+        myLongValue = 2_147_483_647_234L;
+        System.out.println(myLongValue);
+
     }
 }
