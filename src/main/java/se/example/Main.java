1f89bcbbbd35683ab64c9610f3339b623e9f8061
Freddie Molina <freddiejmo@gmail.com>
2023-05-13 21:45:32 -0500

    1-int primitive type

--- src/main/java/se/example/Main.java
+++ src/main/java/se/example/Main.java
@@ -1,10 +1,20 @@
 package se.example;
 
 /**
- * Hello World sample
+ * Primitive Types sample
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello World!");
+        int myValue = 10000;
+        System.out.println(myValue);
+        int myMinIntValue = Integer.MIN_VALUE;
+        System.out.println("Integer minimum value: " + myMinIntValue);
+        int myMaxIntValue = Integer.MAX_VALUE;
+        System.out.println("Integer maximum value: " + myMaxIntValue);
+        System.out.println("Integer Value Range (" + Integer.MIN_VALUE + " to " + Integer.MAX_VALUE + ")");
+        System.out.println("Busted Max value = " + (myMaxIntValue + 1)); // overflow wraparound
+        System.out.println("Busted Min value = " + (myMinIntValue - 1)); // underflow wraparound
+        int myMaxIntTest = 2_147_483_647;
+        System.out.println(myMaxIntTest);
     }
 }
