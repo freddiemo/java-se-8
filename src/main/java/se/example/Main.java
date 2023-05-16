11a65862a042bc22006d1c3972eb45e5be2901a4
Freddie Molina <freddiejmo@gmail.com>
2023-05-16 18:40:30 -0500

    4-megabytes converter

--- src/main/java/se/example/Main.java
+++ src/main/java/se/example/Main.java
@@ -5,23 +5,14 @@ package se.example;
  */
 public class Main {
     public static void main(String[] args) {
-        printConversion(1.5);
+        printMegaBytesAndKiloBytes(0);
     }
 
-    public static long toMilesPerHour(double kilometersPerHour) {
-        long rounded = 0;
-        if (kilometersPerHour < 0) {
-            rounded = -1;
-        } else {
-            rounded = (long) Math.round(kilometersPerHour * 0.62150404);
-        }
-        return rounded;
-    }
-
-    public static void printConversion(double kilometersPerHour) {
-        long milesPerHour = toMilesPerHour(kilometersPerHour);
-        if (milesPerHour >= 0) {
-            System.out.println(kilometersPerHour + " km/h = " + milesPerHour + " mi/h");
+    public static void printMegaBytesAndKiloBytes(int kiloBytes) {
+        int measure = 1024;
+        double megaBytes = kiloBytes / measure;
+        if (kiloBytes >=0) {
+            System.out.println(kiloBytes + " KB = " + Math.round(megaBytes)+ " MB and " + kiloBytes % measure + " KB");
         } else {
             System.out.println("Invalid Value");
         }
