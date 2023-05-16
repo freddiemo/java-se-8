cb6035c16deec63cce9b47ac0e50cf0e32ec35b8
Freddie Molina <freddiejmo@gmail.com>
2023-05-15 22:17:47 -0500

    3-operators precedence

--- src/main/java/se/example/Main.java
+++ src/main/java/se/example/Main.java
@@ -1,46 +1,17 @@
 package se.example;
 
 /**
- * Operators, operands and expressions samples.
+ * Operators precedence sample.
  */
 public class Main {
     public static void main(String[] args) {
-
-        int result = 1;
-        result++;
-        System.out.println("result = " + result);
-
-        result = 1;
-        result -= 1;
-        System.out.println("Result = " + result);
-
-        result = 1;
-        result += 5;
-        System.out.println("Result = " + result);
-
-        result = 10;
-        result -= 2;
-        System.out.println("Result = " + result);
-
-        result = 10;
-        result -= 5.5;
-        System.out.println("Result = " + result);
-
-        result = 10;
-        // sresult = result - 5.5;
-        result = (int) (result - 5.5);
-        System.out.println("Result = " + result);
-
-        double doubleResult = 10;
-        doubleResult -= 5.5;
-        System.out.println("Double result = " + doubleResult);
-
-        result = 10;
-        result *= 1.5;
-        System.out.println("Result = " + result);
-
-        doubleResult = 10;
-        doubleResult /= 1.5;
-        System.out.print("Double result = " + doubleResult);
+        double doubleValue = 20d;
+        double doubleSecondValue = 80d;
+        double result = (doubleValue + doubleSecondValue) * 100d % 40d;
+        boolean hasRemainder = (result != 0) ? true : false;
+        System.out.println("Has remainder: " + hasRemainder);
+        if (hasRemainder) {
+            System.out.println("Got some remainder: " + result);
+        }
     }
 }
