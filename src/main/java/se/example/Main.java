6f4a4632707ea68017a11745b00f271c4c812f08
Freddie Molina <freddiejmo@gmail.com>
2023-05-15 20:19:40 -0500

    2-abbreviating operators

--- src/main/java/se/example/Main.java
+++ src/main/java/se/example/Main.java
@@ -5,22 +5,42 @@ package se.example;
  */
 public class Main {
     public static void main(String[] args) {
-        int result = 1 + 2; // 1 + 2 = 3
-        System.out.println("Result " + result);
-        int previousResult = result;
-        System.out.println("Previous result " + previousResult);
-        result = result - 1; // 3 - 1 = 2
-        System.out.println("Result " + result);
-        char firstChar = 'A'; char secondChar = 'B';
-        System.out.println(firstChar + secondChar);
-        System.out.println("" + firstChar + secondChar);
-        result = 2;
-        result = result * 10; // 2 * 10 = 20
-        System.out.println(result);
-        result = result / 4; // 40 / 4 = 5
-        System.out.println(result);
-        result = 5;
-        result = result % 3; // the remainder of (5 % 3) = 2
-        System.out.println(result);
+
+        int result = 1;
+        result++;
+        System.out.println("result = " + result);
+
+        result = 1;
+        result -= 1;
+        System.out.println("Result = " + result);
+
+        result = 1;
+        result += 5;
+        System.out.println("Result = " + result);
+
+        result = 10;
+        result -= 2;
+        System.out.println("Result = " + result);
+
+        result = 10;
+        result -= 5.5;
+        System.out.println("Result = " + result);
+
+        result = 10;
+        // sresult = result - 5.5;
+        result = (int) (result - 5.5);
+        System.out.println("Result = " + result);
+
+        double doubleResult = 10;
+        doubleResult -= 5.5;
+        System.out.println("Double result = " + doubleResult);
+
+        result = 10;
+        result *= 1.5;
+        System.out.println("Result = " + result);
+
+        doubleResult = 10;
+        doubleResult /= 1.5;
+        System.out.print("Double result = " + doubleResult);
     }
 }
