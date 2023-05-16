b6315cb47b7c3efbb9d235fecdb22cffe19aa77b
Freddie Molina <freddiejmo@gmail.com>
2023-05-15 22:00:40 -0500

    2-ternary operator

--- src/main/java/se/example/Main.java
+++ src/main/java/se/example/Main.java
@@ -1,44 +1,21 @@
 package se.example;
 
 /**
- * If conditionals samples.
+ * Ternary operator samples.
  */
 public class Main {
     public static void main(String[] args) {
-        boolean isAlien = false;
-        if (isAlien == false)
-            System.out.println("It it not alien!");
-
-        int topScore = 100;
-        if (topScore == 100) {
-            System.out.println("You got the high score!");
-        }
-        if (topScore != 100) {
-            System.out.println("You didn't get the high score!");
+        String makeOfCar = "Volkswagen";
+        boolean isDomestic = makeOfCar == "Volkswagen" ? false : true; // isDomestic = (makeOfCar != "Volkswagen")
+        if (isDomestic) {
+            System.out.println("This car is domestic to our country");
         }
 
-        topScore = 80;
-        if (topScore < 100) {
-            System.out.println("You didn't get the high score!");
-        }
+        int ageOfClient = 20;
+        String ageText = (ageOfClient >= 18) ? "Over Eighteen" : "Still a kid";
+        System.out.println("Our client is " + ageText);
 
-        int secondTopScore = 60;
-        if (topScore > secondTopScore && topScore < 100) {
-            System.out.println("Greater than second top score and less than 100");
-        }
-
-        if ((topScore > 90) || (secondTopScore <= 90)) {
-            System.out.println("Either or both of the conditions are true");
-        }
-
-        int newValue = 50;
-        if (newValue == 50) {
-            System.out.println("This is true");
-        }
-
-        boolean isCar = false;
-        if (!isCar) {
-            System.out.println("This is not supposed to happen");
-        }
+        String s = (isDomestic) ? "This car is domestic" : "This car is not domestic";
+        System.out.println(s);
     }
 }
