adb5adee1a20901d26c15845f53df29e2f23e3ce
Freddie Molina <freddiejmo@gmail.com>
2023-05-16 21:14:25 -0500

    10-method overloading

--- src/main/java/se/example/Main.java
+++ src/main/java/se/example/Main.java
@@ -1,27 +1,64 @@
 package se.example;
 
 /**
- * Methods samples.
+ * Methods overloading samples.
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println(hasTeen(22, 23, 34));
-        System.out.println(isTeen(13));
+        System.out.println("New score is " + calculateScore("Tim", 500));
+        System.out.println("New score is " + calculateScore(10));
+
+        System.out.println("5ft, 8in = " + convertToCentimeters(5, 8) + "cm");
+        System.out.println("68in = " + convertToCentimeters(68) + "cm");
+
+        System.out.println(getDurationString(3945));
+        System.out.println(getDurationString(65, 45));
+        System.out.println(getDurationString(-3945));
+        System.out.println(getDurationString(-65, 45));
+        System.out.println(getDurationString(65, 145));
     }
 
-    public static boolean hasTeen(int a, int b, int c) {
-        boolean hasTeen = false;
-        if ((a >= 13 && a <= 19) || (b >= 13 && b <= 19) || (c >= 13 && c <= 19)) {
-            hasTeen = true;
+    public static int calculateScore(String playerName, int score) {
+        System.out.println("Player " + playerName + " scored " + score + " points");
+        return score * 1000;
+    }
+
+    public static int calculateScore(int score) {
+        return calculateScore("Anonymous", score);
+    }
+
+    public static int calculateScore() {
+        System.out.println("No player name, no player score");
+        return 0;
+    }
+
+    public static double convertToCentimeters(int inches) {
+        return inches * 2.54;
+    }
+
+    public static double convertToCentimeters(int feet, int inches) {
+        return convertToCentimeters((feet * 12) + inches);
+    }
+
+    public static String getDurationString(int seconds) {
+        if (seconds < 0 ) {
+            return "Invalid data for seconds (" + seconds + "), must be a positive integer value";
         }
-        return hasTeen;
+
+        return getDurationString(seconds/60, seconds % 60);
     }
 
-    public static boolean isTeen(int number) {
-        boolean isTeen = false;
-        if (number >= 13 && number <= 19)
-            isTeen = true;
-        return isTeen;
+    public static String getDurationString(int minutes, int seconds) {
+        if (minutes < 0) {
+            return "Invalid data for minutes (" + minutes + "), must be a positive integer value";
+        }
+
+        if (seconds < 0 || seconds > 59) {
+            return "Invalid data for seconds (" + seconds + "), must be a value between 1 and 59";
+        }
+
+        return minutes/60 + "h " + minutes % 60 + "m " + seconds + "s";
     }
+
 
 }
