3664b22f472d6a3a62ae340430f01f1222f5bbcc
Freddie Molina <freddiejmo@gmail.com>
2023-05-16 19:41:01 -0500

    9-teen number checker

--- src/main/java/se/example/Main.java
+++ src/main/java/se/example/Main.java
@@ -5,14 +5,23 @@ package se.example;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println(hasEqualSum(1, -1, 0));
+        System.out.println(hasTeen(22, 23, 34));
+        System.out.println(isTeen(13));
     }
 
-    public static boolean hasEqualSum(int a, int b, int c) {
-        boolean equalSum = false;
-        if (a + b == c)
-            equalSum = true;
-        return equalSum;
+    public static boolean hasTeen(int a, int b, int c) {
+        boolean hasTeen = false;
+        if ((a >= 13 && a <= 19) || (b >= 13 && b <= 19) || (c >= 13 && c <= 19)) {
+            hasTeen = true;
+        }
+        return hasTeen;
+    }
+
+    public static boolean isTeen(int number) {
+        boolean isTeen = false;
+        if (number >= 13 && number <= 19)
+            isTeen = true;
+        return isTeen;
     }
 
 }
