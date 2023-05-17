4b77a7c23667dc29b06d43b01a84c1d55f9d2836
Freddie Molina <freddiejmo@gmail.com>
2023-05-17 17:52:31 -0500

    9-perfect number

--- src/main/java/se/example/Main.java
+++ src/main/java/se/example/Main.java
@@ -5,25 +5,29 @@ package se.example;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println(getGreatestCommonDivisor(25, 15));
-        System.out.println(getGreatestCommonDivisor(12, 30));
-        System.out.println(getGreatestCommonDivisor(9, 18));
-        System.out.println(getGreatestCommonDivisor(81, 153));
+        System.out.println(isPerfectNumber(6));
+        System.out.println(isPerfectNumber(28));
+        System.out.println(isPerfectNumber(5));
+        System.out.println(isPerfectNumber(-1));
     }
 
-    public static int getGreatestCommonDivisor(int first, int second) {
-        if (first < 10 || second < 10) {
-            return -1;
+    public static boolean isPerfectNumber(int number) {
+        if (number < 1) {
+            return false;
         }
-        int maxDivisor = (first < second) ? first : second;
+        int sumDivisors = 0;
+        int divisor = number -1;
         do {
-            if ((first % maxDivisor == 0) && (second % maxDivisor == 0)) {
-                return maxDivisor;
+            if (number % divisor == 0) {
+                sumDivisors += divisor;
             }
-            maxDivisor--;
-        } while(maxDivisor > 0);
+            divisor--;
+        } while(divisor > 0);
+        if (sumDivisors == number) {
+            return true;
+        }
 
-        return maxDivisor;
+        return false;
     }
 
 }
