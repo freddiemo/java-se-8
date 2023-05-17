323fd31eb298f9d733232d77a8f19bcc66619365
Freddie Molina <freddiejmo@gmail.com>
2023-05-17 16:25:50 -0500

    7-all-factors

--- src/main/java/se/example/Main.java
+++ src/main/java/se/example/Main.java
@@ -5,32 +5,24 @@ package se.example;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println(isValid(10));
-        System.out.println(isValid(468));
-        System.out.println(isValid(1051));
-
-        System.out.println(hasSameLastDigit(41, 22, 71));
-        System.out.println(hasSameLastDigit(23, 32, 42));
-        System.out.println(hasSameLastDigit(9, 99, 999));
+        printFactors(6);
+        printFactors(32);
+        printFactors(10);
+        printFactors(-1);
     }
 
-    public static boolean hasSameLastDigit(int a, int b, int c) {
-
-        if (isValid(a) && isValid(b) && isValid(c)) {
-            if (((a % 10) == (b % 10)) || ((a % 10) == (c % 10)) || ((b % 10) == (c % 10))) {
-                return true;
+    public static void printFactors(int number) {
+        if (number < 1) {
+            System.out.println("Invalid Value");
+        } else {
+            int factor = 1;
+            while(factor <= number) {
+                if (number % factor == 0) {
+                    System.out.println(factor);
+                }
+                factor++;
             }
         }
-
-        return false;
-    }
-
-    public static boolean isValid(int number) {
-        if (number >= 10 && number <= 1000) {
-            return true;
-        }
-
-        return false;
     }
 
 }
