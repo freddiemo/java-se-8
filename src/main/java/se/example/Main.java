d88138342ae54ca03b38ee7c67d915bd5641f9d1
Freddie Molina <freddiejmo@gmail.com>
2023-05-17 15:40:46 -0500

    4-event-digit-sum

--- src/main/java/se/example/Main.java
+++ src/main/java/se/example/Main.java
@@ -5,29 +5,25 @@ package se.example;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println(sumFirstAndLastDigit(252));
-        System.out.println(sumFirstAndLastDigit(257));
-        System.out.println(sumFirstAndLastDigit(0));
-        System.out.println(sumFirstAndLastDigit(5));
-        System.out.println(sumFirstAndLastDigit(-10));
+        System.out.println(getEvenDigitSum(123456789));
+        System.out.println(getEvenDigitSum(252));
+        System.out.println(getEvenDigitSum(-22));
     }
 
-    public static int sumFirstAndLastDigit(int number) {
+    public static int getEvenDigitSum(int number) {
         if (number < 0) {
             return -1;
         }
-        if (number < 9) {
-            return number * 2;
-        }
-        int firstNumber = 0;
-        int lastNumber = number % 10;
-        while(number > 9) {
-            number /= 10;
-            if (number <= 9) {
-                firstNumber = number;
+        int sum = 0;
+        while(number > 0) {
+            int lastNumber = (number % 10);
+            if (lastNumber % 2 == 0) {
+                sum += lastNumber;
             }
+            number /= 10;
         }
-        return firstNumber + lastNumber;
+
+        return sum;
     }
 
 }
