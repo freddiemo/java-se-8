65f47e9bf3a2b3798057bfdc503de55c59cc1217
Freddie Molina <freddiejmo@gmail.com>
2023-05-17 20:02:39 -0500

    15-flour pack

--- src/main/java/se/example/Main.java
+++ src/main/java/se/example/Main.java
@@ -5,18 +5,22 @@ package se.example;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println(isCatPlaying(true, 10));
-        System.out.println(isCatPlaying(false, 36));
-        System.out.println(isCatPlaying(false, 35));
-        System.out.println(isCatPlaying(true, 45));
+        System.out.println(canPack(1, 0, 4));
+        System.out.println(canPack(1, 0, 5));
+        System.out.println(canPack(0, 5, 4));
+        System.out.println(canPack(2, 2, 11));
+        System.out.println(canPack(-3, 2, 12));
+        System.out.println(canPack(2, 1, 5));
     }
 
-    public static boolean isCatPlaying(boolean summer, int temperature) {
-        if ((temperature >= 25 && temperature <= 35) || summer && (temperature >= 25 && temperature <= 45)) {
-            return true;
-        } else {
+    public static boolean canPack(int bigCount, int smallCount, int goal) {
+        if (bigCount < 0 || smallCount < 0 || goal < 0) {
             return false;
         }
+        if (bigCount * 5 <= goal && bigCount * 5 + smallCount >= goal) {
+            return true;
+        }
+        return false;
     }
 
 }
