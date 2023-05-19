4f412a5bd17238fdd3a9ff1b5d915f69799e6034
Freddie Molina <freddiejmo@gmail.com>
2023-05-19 17:00:39 -0500

    11-4-person class sample

--- src/main/java/se/example/Main.java
+++ src/main/java/se/example/Main.java
@@ -1,21 +1,24 @@
 package se.example;
 
-import se.example.classes_11.SimpleCalculator3;
+import se.example.classes_11.Person4;
 
 /**
  * Classes sample
  */
 public class Main {
     public static void main(String[] args) {
-        SimpleCalculator3 calculator = new SimpleCalculator3();
-        calculator.setFirstNumber(5.0);
-        calculator.setSecondNumber(4);
-        System.out.println("add= " + calculator.getAdditionResult());
-        System.out.println("subtract= " + calculator.getSubtractionResult());
-        calculator.setFirstNumber(5.25);
-        calculator.setSecondNumber(0);
-        System.out.println("multiply= " + calculator.getMultiplicationResult());
-        System.out.println("divide= " + calculator.getDivisionResult());
+        Person4 person = new Person4();
+        person.setFirstName("");   // firstName is set to empty string
+        person.setLastName("");    // lastName is set to empty string
+        person.setAge(10);
+        System.out.println("fullName= " + person.getFullName());
+        System.out.println("teen= " + person.isTeen());
+        person.setFirstName("John");    // firstName is set to John
+        person.setAge(18);
+        System.out.println("fullName= " + person.getFullName());
+        System.out.println("teen= " + person.isTeen());
+        person.setLastName("Smith");    // lastName is set to Smith
+        System.out.println("fullName= " + person.getFullName());
     }
 
 }
