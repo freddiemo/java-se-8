808d230f31f90f82fcee014533de469be7f7ce5c
Freddie Molina <freddiejmo@gmail.com>
2023-05-18 20:16:35 -0500

    10-5-paint job

--- src/main/java/se/example/Main.java
+++ src/main/java/se/example/Main.java
@@ -1,15 +1,23 @@
 package se.example;
 
-import se.example.scanner_console_10.InputCalculator4;
-import se.example.scanner_console_10.MinMaxChallenge3;
-import se.example.scanner_console_10.ReadingUserInputChallenge2;
+import se.example.scanner_console_10.PaintJob5;
 
 /**
  * Inputs console sample
  */
 public class Main {
     public static void main(String[] args) {
-        InputCalculator4.inputThenPrintSumAndAverage();
+        /*System.out.println(PaintJob5.getBucketCount(3.4, 1.5));
+        System.out.println(PaintJob5.getBucketCount(6.26, 2.2));
+        System.out.println(PaintJob5.getBucketCount(3.26, 0.75));*/
+
+        /*System.out.println(PaintJob5.getBucketCount(-3.4, 2.1, 1.5));
+        System.out.println(PaintJob5.getBucketCount(3.4, 2.1, 1.5));
+        System.out.println(PaintJob5.getBucketCount(7.25, 4.3, 2.35));*/
+
+        System.out.println(PaintJob5.getBucketCount(-3.4, 2.1, 1.5, 2));
+        System.out.println(PaintJob5.getBucketCount(3.4, 2.1, 1.5, 2));
+        System.out.println(PaintJob5.getBucketCount(2.75, 3.25, 2.5, 1));
     }
 
 }
