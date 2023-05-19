6de38b93874659923e18aa79b1727a77e70b1d91
Freddie Molina <freddiejmo@gmail.com>
2023-05-19 16:21:31 -0500

    11-3-simple calculator

--- src/main/java/se/example/Main.java
+++ src/main/java/se/example/Main.java
@@ -1,31 +1,21 @@
 package se.example;
 
-import se.example.classes_11.BankAccount2;
+import se.example.classes_11.SimpleCalculator3;
 
 /**
  * Classes sample
  */
 public class Main {
     public static void main(String[] args) {
-        BankAccount2 account = new BankAccount2();
-
-        account.setNumber("12345");
-        account.setBalance(1000d);
-        account.setCustomerName("Bob Brown");
-        account.setCustomerEmail("myemail@bobo.com");
-        account.setCustomerPhoneNumber("(087) 123-4567");
-
-        account.withdrawFunds(100);
-        account.depositFunds(250);
-        account.withdrawFunds(50);
-
-        account.withdrawFunds(200);
-
-        account.depositFunds(100);
-        account.withdrawFunds(45.55);
-        account.withdrawFunds(54.46);
-
-        account.withdrawFunds(54.45);
+        SimpleCalculator3 calculator = new SimpleCalculator3();
+        calculator.setFirstNumber(5.0);
+        calculator.setSecondNumber(4);
+        System.out.println("add= " + calculator.getAdditionResult());
+        System.out.println("subtract= " + calculator.getSubtractionResult());
+        calculator.setFirstNumber(5.25);
+        calculator.setSecondNumber(0);
+        System.out.println("multiply= " + calculator.getMultiplicationResult());
+        System.out.println("divide= " + calculator.getDivisionResult());
     }
 
 }
