a5017dabd1224ad3bb7b331824da559e3b1ce4bc
Freddie Molina <freddiejmo@gmail.com>
2023-05-19 21:05:16 -0500

    11-10-carpet cost calculator

--- src/main/java/se/example/Main.java
+++ src/main/java/se/example/Main.java
@@ -1,21 +1,23 @@
 package se.example;
 
 
-import se.example.classes_11.Point8;
+import se.example.classes_11.Floor9;
+import se.example.classes_11.Carpet9;
+import se.example.classes_11.Calculator9;
 
 /**
  * Classes samples.
  */
 public class Main {
     public static void main(String[] args) {
-
-        Point8 first = new Point8(6, 5);
-        Point8 second = new Point8(3, 1);
-        System.out.println("distance(0,0)= " + first.distance());
-        System.out.println("distance(second)= " + first.distance(second));
-        System.out.println("distance(2,2)= " + first.distance(2, 2));
-        Point8 point = new Point8();
-        System.out.println("distance()= " + point.distance());
+        Carpet9 carpet = new Carpet9(3.5);
+        Floor9 floor = new Floor9(2.75, 4.0);
+        Calculator9 calculator = new Calculator9(floor, carpet);
+        System.out.println("total= " + calculator.getTotalCost());
+        carpet = new Carpet9(1.5);
+        floor = new Floor9(5.4, 4.5);
+        calculator = new Calculator9(floor, carpet);
+        System.out.println("total= " + calculator.getTotalCost());
     }
 
 }
