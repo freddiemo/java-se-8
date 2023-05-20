e3f40ac406a75ca44c0d72548189db1148dbcd28
Freddie Molina <freddiejmo@gmail.com>
2023-05-19 19:57:42 -0500

    11-7-POJO

--- src/main/java/se/example/Main.java
+++ src/main/java/se/example/Main.java
@@ -1,27 +1,30 @@
 package se.example;
 
-import se.example.classes_11.Customer5;
+
+import se.example.classes_11.Student6;
 
 /**
- * Classes sample
+ * Classes samples.
  */
 public class Main {
     public static void main(String[] args) {
 
-        Customer5 customer = new Customer5("Tim", 1000d, "tim@email.com");
-        System.out.println(customer.getName());
-        System.out.println(customer.getCreditLimit());
-        System.out.println(customer.getEmail());
-
-        Customer5 secondCustomer = new Customer5();
-        System.out.println(secondCustomer.getName());
-        System.out.println(secondCustomer.getCreditLimit());
-        System.out.println(secondCustomer.getEmail());
-
-        Customer5 thirdCustomer = new Customer5("Joe", "joe@email.com");
-        System.out.println(thirdCustomer.getName());
-        System.out.println(thirdCustomer.getCreditLimit());
-        System.out.println(thirdCustomer.getEmail());
+        for(int i = 1; i <= 5; i++) {
+            String name =  "";
+            switch(i) {
+                case 1: name = "Mary"; break;
+                case 2: name = "Carol"; break;
+                case 3: name = "Tim"; break;
+                case 4: name = "Harry"; break;
+                case 5: name = "Lisa"; break;
+                default: name = "Anonymous"; break;
+            };
+            Student6 student = new Student6("S92300" + i,
+                               name,
+                                "05/11/1985",
+                                "Java Masterclass");
+            System.out.println(student);
+        }
     }
 
 }
