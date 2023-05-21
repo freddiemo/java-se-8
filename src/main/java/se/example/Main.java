ea4154c49017bff0fd9a2f034e3fb92d2fd88c71
Freddie Molina <freddiejmo@gmail.com>
2023-05-20 20:49:00 -0500

    13-2-String methods

--- src/main/java/se/example/Main.java
+++ src/main/java/se/example/Main.java
@@ -8,8 +8,33 @@ import se.example.strings_13.Strings;
 public class Main extends Object {
     public static void main(String[] args) {
 
-        Strings.stringFormatting();
+        // Strings.stringFormatting();
+        Strings.printInformation("Hello World");
+        Strings.printInformation("");
+        Strings.printInformation("\t    \n");
 
+        String helloWorld = "Hello World";
+        System.out.printf("index of r = %d %n", Strings.getIndex(helloWorld, 'r'));
+        System.out.printf("index of World = %d %n", Strings.getIndex(helloWorld, "World"));
+
+        System.out.printf("index of l = %d %n", Strings.getIndex(helloWorld, 'l'));
+        System.out.printf("index of l = %d %n", Strings.getLastIndex(helloWorld, 'l'));
+
+        System.out.printf("index of l = %d %n", Strings.getIndex(helloWorld, 'l', 3));
+        System.out.printf("index of l = %d %n", Strings.getLastIndex(helloWorld, 'l', 8));
+
+        String lowerCased = Strings.toLowerCase(helloWorld);
+        Strings.equals(helloWorld, lowerCased);
+        
+        Strings.equalsIgnoreCase(helloWorld, lowerCased);
+
+        Strings.startsWith(helloWorld, "Hello");
+
+        Strings.endsWith(helloWorld, "World");
+
+        Strings.contains(helloWorld, "World");
+
+        Strings.contentEquals(helloWorld, "Hello World");
     }
 
 }
--- src/main/java/se/example/strings_13/Strings.java
+++ src/main/java/se/example/strings_13/Strings.java
@@ -1,6 +1,92 @@
 package se.example.strings_13;
 
 public class Strings {
+
+    /* String comparison methods */
+
+    public static void contentEquals(String string, String compareTo) {
+        if(string.contentEquals(compareTo)) {
+            System.out.println("Values match exactly");
+        }
+    }
+
+    public static void contains(String string, String substring) {
+        if(string.contains(substring)) { 
+            System.out.println("String contains '" + substring + "\'");
+        }
+    }
+
+    public static void endsWith(String string, String substring) {
+        if(string.endsWith(substring)) {
+            System.out.println("String ends with \'" + substring + "\'");
+        }
+    }
+
+    public static void startsWith(String string, String substring) {
+        if(string.startsWith(substring)) {
+            System.out.println("String starts with \'" + substring + "\'");
+        }
+    }
+
+    public static void equalsIgnoreCase(String string, String compareTo) {
+        if(string.equalsIgnoreCase(compareTo)) {
+            System.out.println("Values match ignoring case");
+        } 
+    }
+
+    public static void equals(String string, String compareTo) {
+        if(string.equals(compareTo)) {
+            System.out.println("Values match exactly");
+        } 
+    }
+
+    public static String toLowerCase(String string) {
+        return string.toLowerCase();
+    }
+
+
+
+    /* String inspection methods */
+
+    public static int getLastIndex(String string, char character) {
+        return string.lastIndexOf(character);
+    }
+
+    public static int getLastIndex(String string, char character, int match) {
+        return string.lastIndexOf(character, match);
+    }
+
+
+    public static int getIndex(String string, char character) {
+        return string.indexOf(character);
+    }
+
+    public static int getIndex(String string, char character, int match) {
+        return string.indexOf(character, match);
+    }
+
+    public static int getIndex(String string, String characters) {
+        return string.indexOf(characters);
+    }
+
+    public static void printInformation(String string) {
+        int length = string.length();
+        System.out.printf("Length = %d %n", length);
+
+        if (string.isEmpty()) {
+            System.out.println("String is Empty");
+            return;
+        }
+
+        if (string.isBlank()) {
+            System.out.println("String is Blank");
+        }
+
+        System.out.printf("First char = %c %n", string.charAt(0));
+        System.out.printf("First char = %c %n", string.charAt(length - 1));
+
+    }
+
 	public static void stringFormatting() {
 		String bulletIt = "Print a Bulleted List:\n" +
             "\t\u2022 First Point\n" +
@@ -31,4 +117,5 @@ public class Strings {
         /*formattedString = "Your age is %d".formatted(age);  // since java 15
         System.out.println(formattedString);*/
 	}
+
 }
